d2cb380f1d10d0d1194d266823bb452f8e052559
SPARTAN-175 <avengers_0077@hotmail.com>
2026-09-03 20:01:10 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='eco-v1';
-const CORE=['./','./index.html','./levels.json','./manifest.webmanifest','./assets/icon-192.svg','./assets/icon-512.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
